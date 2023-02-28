d2576bf20a7042bcfe3b6d5cebdf8c879457ffd6
ismaelpaul <ismaafp@gmail.com>
2023-02-28 11:20:45 +0000

    Add /signup route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Auth/Login';
+import SignUp from './pages/Auth/SignUp';
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/login" element={<Login />}></Route>
+			<Route path="/signup" element={<SignUp />}></Route>
 		</Routes>
 	);
 }
