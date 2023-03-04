435912d64e6d914e5653b22c23022b2e2be47899
ismaelpaul <ismaafp@gmail.com>
2023-03-04 09:18:25 +0000

    Add home page route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
 import Login from './pages/Auth/Login';
 import SignUp from './pages/Auth/SignUp';
+import Home from './pages/Home/Home';
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/login" element={<Login />}></Route>
 			<Route path="/signup" element={<SignUp />}></Route>
+			<Route
+				path="/"
+				element={
+					<Nav>
+						<Home />
+					</Nav>
+				}
+			></Route>
 		</Routes>
 	);
 }
