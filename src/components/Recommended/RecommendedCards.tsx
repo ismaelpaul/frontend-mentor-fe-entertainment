e5e515547501e14609fbe060ab83a7fcb7f3af92
ismaelpaul <ismaafp@gmail.com>
2023-03-08 15:38:45 +0000

    Add handleBookmarking function

--- src/components/Recommended/RecommendedCards.tsx
+++ src/components/Recommended/RecommendedCards.tsx
@@ -1,15 +1,31 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { moviesIcon, tvSeriesIcon } from '../../data/icons';
-import { RootState } from '../../redux/store';
+import { AppDispatch, RootState } from '../../redux/store';
 import BookmarkIcon from '../BookmarkIcon/BookmarkIcon';
 import Card from '../Card/Card';
 import '../../styles/cards.scss';
 import { Entertainment } from '../Trending/TrendingCards';
+import {
+	addNewBookmark,
+	getEntertainment,
+	NewBookmark,
+} from '../../redux/features/entertainment/entertainmentSlice';
 
 const RecommendedCards = () => {
+	const dispatch = useDispatch<AppDispatch>();
+
 	const { entertainments } = useSelector(
 		(state: RootState) => state.entertainments
 	);
+
+	const newBookmark = useSelector(
+		(state: RootState) => state.entertainments.singleEntertainment
+	);
+
+	const handleBookmarking = async (id: string, newBookmark: NewBookmark) => {
+		await dispatch(getEntertainment(id));
+		await dispatch(addNewBookmark(newBookmark));
+	};
 
 	return (
 		<div className="infoContainer">
@@ -17,7 +33,12 @@ const RecommendedCards = () => {
 				if (!entertainment.isTrending) {
 					return (
 						<Card cardClass="recommended" key={index}>
-							<span className="bookmarkIcon">
+							<span
+								className="bookmarkIcon"
+								onClick={() => {
+									handleBookmarking(entertainment._id, newBookmark);
+								}}
+							>
 								<BookmarkIcon />
 							</span>
 							<img src={entertainment.thumbnail.regular.small} />
