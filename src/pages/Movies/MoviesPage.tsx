6af9989ff8345faa2a2487fcbcf099068cf40a2a
ismaelpaul <ismaafp@gmail.com>
2023-03-12 09:58:43 +0000

    Add useRedirectLoggedOutUser custom hook

--- src/pages/Movies/MoviesPage.tsx
+++ src/pages/Movies/MoviesPage.tsx
@@ -1,7 +1,10 @@
 import SearchBar from '../../components/SearchBar/SearchBar';
 import Movies from '../../components/Movies/Movies';
+import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser';
 
 const MoviesPage = () => {
+	useRedirectLoggedOutUser('/login');
+
 	const placeholder: string = 'Search for movies';
 	return (
 		<>
