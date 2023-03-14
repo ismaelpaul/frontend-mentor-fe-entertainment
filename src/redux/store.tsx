b4ee82e73e3d035beef194382557850a866d7016
ismaelpaul <ismaafp@gmail.com>
2023-03-14 20:28:13 +0000

    Add filter reducer

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from '../redux/features/auth/authSlice';
-import entertainmentReducer from '../redux/features/entertainment/entertainmentSlice';
+import authReducer from './features/auth/authSlice';
+import entertainmentReducer from './features/entertainment/entertainmentSlice';
+import filterReducer from './features/entertainment/filterSlice';
 
 export const store = configureStore({
 	reducer: {
 		auth: authReducer,
 		entertainments: entertainmentReducer,
+		filter: filterReducer,
 	},
 });
 
