48d89cb287e10028d76533db38ea90aba3366e8f
ismaelpaul <ismaafp@gmail.com>
2023-03-15 16:49:41 +0000

    Add conditional rendering on search

--- src/pages/Bookmarked/BookmarkedPage.tsx
+++ src/pages/Bookmarked/BookmarkedPage.tsx
@@ -1,26 +1,57 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { ChangeEvent, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Bookmarked from '../../components/Bookmarked/Bookmarked';
+import FilteredEntertainments from '../../components/FilteredEntertainments/FilteredEntertainments';
 import SearchBar from '../../components/SearchBar/SearchBar';
 import useRedirectLoggedOutUser from '../../customHook/useRedirectLoggedOutUser';
 import { getBookmarkeds } from '../../redux/features/entertainment/entertainmentSlice';
-import { AppDispatch } from '../../redux/store';
+import { FILTER_BOOKMARKEDS } from '../../redux/features/entertainment/filterSlice';
+import { AppDispatch, RootState } from '../../redux/store';
 
 const BookmarkedPage = () => {
+	const [search, setSearch] = useState('');
+	const [isSearching, setIsSearching] = useState(false);
+
 	useRedirectLoggedOutUser('/login');
 
 	const placeholder: string = 'Search for bookmarked shows';
 
 	const dispatch = useDispatch<AppDispatch>();
 
+	const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+		setSearch(e.target.value);
+	};
+
+	const bookmarkeds = useSelector(
+		(state: RootState) => state.entertainments.bookmarkeds
+	);
+
 	useEffect(() => {
 		dispatch(getBookmarkeds());
 	}, []);
 
+	useEffect(() => {
+		dispatch(FILTER_BOOKMARKEDS({ bookmarkeds, search }));
+
+		if (search !== '') {
+			setIsSearching(true);
+		} else {
+			setIsSearching(false);
+		}
+	}, [bookmarkeds, search]);
+
 	return (
 		<>
-			<SearchBar placeholder={placeholder} />
-			<Bookmarked />
+			<SearchBar
+				placeholder={placeholder}
+				search={search}
+				handleSearch={(e) => handleSearch(e)}
+			/>
+			{isSearching ? (
+				<FilteredEntertainments search={search} />
+			) : (
+				<Bookmarked />
+			)}
 		</>
 	);
 };
