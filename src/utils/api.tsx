f04e5aea20b86f415b0cc3298f62af425d22d34e
ismaelpaul <ismaafp@gmail.com>
2023-03-22 16:45:38 +0000

    Add deleteBookmark api function

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -9,7 +9,6 @@ type UserData = {
 const entertainmentApi = axios.create({
 	baseURL: 'http://localhost:9090/api',
 	withCredentials: true,
-	credentials: 'include',
 });
 
 export const getAllEntertainments = async () => {
@@ -26,6 +25,12 @@ export const getAllBookmarkeds = async () => {
 
 export const addBookmark = async (newBookmark: NewBookmark) => {
 	const response = await entertainmentApi.post('/bookmarks', newBookmark);
+
+	return response.data;
+};
+
+export const deleteBookmark = async (id: string) => {
+	const response = await entertainmentApi.delete(`/bookmarks/${id}`);
 
 	return response.data;
 };
@@ -49,7 +54,7 @@ export const loginUser = async (userData: UserData) => {
 };
 
 export const getLoginStatus = async () => {
-	const response = await entertainmentApi.get('/user//loggedin');
+	const response = await entertainmentApi.get('/user/loggedin');
 
 	return response.data;
 };
