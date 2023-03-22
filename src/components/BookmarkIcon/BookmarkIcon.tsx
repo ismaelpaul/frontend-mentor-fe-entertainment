2eb1f9a849d6e8e98a90cb55b4fc2c583228a18c
ismaelpaul <ismaafp@gmail.com>
2023-03-22 16:52:40 +0000

    Add handleBookmarking function

--- src/components/BookmarkIcon/BookmarkIcon.tsx
+++ src/components/BookmarkIcon/BookmarkIcon.tsx
@@ -1,3 +1,9 @@
+import { useDispatch } from 'react-redux';
+import {
+	addNewBookmark,
+	getEntertainment,
+} from '../../redux/features/entertainment/entertainmentSlice';
+import { AppDispatch } from '../../redux/store';
 import styles from './BookmarkIcon.module.scss';
 
 const bookmarkIcon = (
@@ -5,14 +11,35 @@ const bookmarkIcon = (
 		<path
 			d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
 			stroke="#FFF"
-			stroke-width="1.5"
+			strokeWidth="1.5"
 			fill="none"
 		/>
 	</svg>
 );
+type IconProps = {
+	id: string;
+};
 
-const BookmarkIcon = () => {
-	return <div className={styles.iconBackground}>{bookmarkIcon}</div>;
+const BookmarkIcon = ({ id }: IconProps) => {
+	const dispatch = useDispatch<AppDispatch>();
+
+	const handleBookmarking = async (id: string) => {
+		const newBookmark = await dispatch(getEntertainment(id));
+
+		dispatch(addNewBookmark(newBookmark.payload));
+	};
+	return (
+		<div className={styles.bookmarkIcon}>
+			<button
+				className={styles.iconBackground}
+				onClick={() => {
+					handleBookmarking(id);
+				}}
+			>
+				{bookmarkIcon}
+			</button>
+		</div>
+	);
 };
 
 export default BookmarkIcon;
