1059818eff6f872bab1726e3e5efe5f660369abe
ismaelpaul <ismaafp@gmail.com>
2023-03-22 16:56:24 +0000

    Add InitialStateType and reducers

--- src/redux/features/auth/authSlice.tsx
+++ src/redux/features/auth/authSlice.tsx
@@ -1,15 +1,41 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
+type InitialStateType = {
+	isLoggedIn: boolean;
+	user: {
+		email: string;
+		picture: string;
+	};
+};
+
+const initialState: InitialStateType = {
 	isLoggedIn: false,
+	user: {
+		email: '',
+		picture: '',
+	},
 };
 
 const authSlice = createSlice({
 	name: 'auth',
 	initialState,
-	reducers: {},
+	reducers: {
+		SET_LOGIN(state, action) {
+			state.isLoggedIn = action.payload;
+		},
+		SET_USER(state, action) {
+			const profile = action.payload;
+			state.user.email = profile.email;
+			state.user.picture = profile.picture;
+		},
+	},
 });
 
-export const {} = authSlice.actions;
+export const { SET_LOGIN, SET_USER } = authSlice.actions;
+
+export const selectIsLoggedIn = (state: { auth: InitialStateType }) =>
+	state.auth.isLoggedIn;
+export const selectUser = (state: { auth: InitialStateType }) =>
+	state.auth.user;
 
 export default authSlice.reducer;
