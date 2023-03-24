24a2a0640a727fc7c89f27a419584334135711c6
ismaelpaul <ismaafp@gmail.com>
2023-03-24 09:55:13 +0000

    Add interfaces

--- src/redux/features/entertainment/filterSlice.tsx
+++ src/redux/features/entertainment/filterSlice.tsx
@@ -1,4 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { Entertainment } from '../../../components/Trending/TrendingCards';
+import { NewBookmark } from './entertainmentSlice';
 
 interface InitialState {
 	filter: {
@@ -24,39 +26,47 @@ const filterSlice = createSlice({
 		FILTER_ENTERTAINMENTS(state, action) {
 			const { entertainments, search } = action.payload;
 
-			const tempEntertainments = entertainments.filter((entertainment) => {
-				return entertainment.title.toLowerCase().includes(search.toLowerCase());
-			});
+			const tempEntertainments = entertainments.filter(
+				(entertainment: Entertainment) => {
+					return entertainment.title
+						.toLowerCase()
+						.includes(search.toLowerCase());
+				}
+			);
 			state.filteredEntertainments = tempEntertainments;
 		},
 		FILTER_MOVIES(state, action) {
 			const { entertainments, search } = action.payload;
 
-			const tempMovies = entertainments.filter((entertainment) => {
-				if (entertainment.category === 'Movie') {
-					return entertainment.title
-						.toLowerCase()
-						.includes(search.toLowerCase());
+			const tempMovies = entertainments.filter(
+				(entertainment: Entertainment) => {
+					if (entertainment.category === 'Movie') {
+						return entertainment.title
+							.toLowerCase()
+							.includes(search.toLowerCase());
+					}
 				}
-			});
+			);
 			state.filteredMovies = tempMovies;
 		},
 		FILTER_TV_SERIES(state, action) {
 			const { entertainments, search } = action.payload;
 
-			const tempTvSeries = entertainments.filter((entertainment) => {
-				if (entertainment.category === 'TV Series') {
-					return entertainment.title
-						.toLowerCase()
-						.includes(search.toLowerCase());
+			const tempTvSeries = entertainments.filter(
+				(entertainment: Entertainment) => {
+					if (entertainment.category === 'TV Series') {
+						return entertainment.title
+							.toLowerCase()
+							.includes(search.toLowerCase());
+					}
 				}
-			});
+			);
 			state.filteredTvSeries = tempTvSeries;
 		},
 		FILTER_BOOKMARKEDS(state, action) {
 			const { bookmarkeds, search } = action.payload;
 
-			const tempBookmarkeds = bookmarkeds.filter((bookmark) => {
+			const tempBookmarkeds = bookmarkeds.filter((bookmark: NewBookmark) => {
 				return bookmark.title.toLowerCase().includes(search.toLowerCase());
 			});
 			state.filteredBookmarks = tempBookmarkeds;
