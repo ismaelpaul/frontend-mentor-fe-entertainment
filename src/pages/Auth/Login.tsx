acea577ffa3f695f15227d57ec02014590f86a7e
ismaelpaul <ismaafp@gmail.com>
2023-03-27 20:33:06 +0100

    Replace div for main tag

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -50,7 +50,7 @@ const Login = () => {
 	};
 
 	return (
-		<div className={styles.auth}>
+		<main className={styles.auth}>
 			<svg width="32" height="25.6" xmlns="http://www.w3.org/2000/svg">
 				<path
 					d="m26.463.408 3.2 6.4h-4.8l-3.2-6.4h-3.2l3.2 6.4h-4.8l-3.2-6.4h-3.2l3.2 6.4h-4.8l-3.2-6.4h-1.6a3.186 3.186 0 0 0-3.184 3.2l-.016 19.2a3.2 3.2 0 0 0 3.2 3.2h25.6a3.2 3.2 0 0 0 3.2-3.2V.408h-6.4Z"
@@ -95,7 +95,7 @@ const Login = () => {
 					</Link>{' '}
 				</p>
 			</Card>
-		</div>
+		</main>
 	);
 };
 
