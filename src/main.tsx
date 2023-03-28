895a49d1cbeb3a67e63c71d2bc2633398c4b48ca
ismaelpaul <ismaafp@gmail.com>
2023-03-28 20:58:39 +0100

    Add ToastContainer component

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import App from './App';
 import './index.scss';
 import { store } from './redux/store';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<BrowserRouter>
+				<ToastContainer autoClose={2000} />
 				<App />
 			</BrowserRouter>
 		</Provider>
